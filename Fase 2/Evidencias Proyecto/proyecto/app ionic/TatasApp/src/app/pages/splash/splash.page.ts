f0059d07a5bb54e4aa9f49d5314d1acfe60e9670
David Guentelican <david.guentelican@gmail.com>
2025-04-23 00:23:44 -0400

    validacion si existe token registrado navega a principal, si no navega a login

--- Fase 2/Evidencias Proyecto/proyecto/app ionic/TatasApp/src/app/pages/splash/splash.page.ts
+++ Fase 2/Evidencias Proyecto/proyecto/app ionic/TatasApp/src/app/pages/splash/splash.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { DbOffService } from 'src/app/services/db-off.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -17,28 +18,47 @@ export class SplashPage implements OnInit {
   tokenRegistrado: string = "";
 
   constructor(
-    private dbOff: DbOffService
+    private dbOff: DbOffService,
+    private router: Router
   ) { }
 
   async ngOnInit() {
-    this.dbOff.abrirDB();
-    this.dbOff.crearTablaUsuario();
+    this.dbOff.abrirDB(); //habilita la base de datos
+    this.dbOff.crearTablaUsuario(); //crea o valida tabla usuario
+    this.tokenRegistrado = ""; //vaciar por seguridad
 
     //validar que haya token de usuario registrado
     //creado por david el 23/04
     let usuario = await this.dbOff.obtenerDatosUsuarioLogueado();
 
     if (usuario) {
+      //si hay token navega al principal
       this.tokenRegistrado = usuario.token;
       console.log("tatas: token registrado ", this.tokenRegistrado);
+      setTimeout(() => {
+        this.navegarPrincipal();
+      }, 1500);
     } else {
+      //si no hay token navega al login
       console.log("tatas: no hay token registrado");
+      setTimeout(() => {
+        this.navegarLogin();
+      }, 1500);
     }
   }
 
+  //funcion para navegar al login
+  //creada por david el 23/04
+  navegarLogin() {
+    let extras: NavigationExtras = {replaceUrl: true};
+    this.router.navigate(["login"], extras);
+  }
 
-  // navegarLogin() {
-
-  // }
+  //funcion para navegar al principal
+  //creada por david el 23/04
+  navegarPrincipal() {
+    let extras: NavigationExtras = {replaceUrl: true};
+    this.router.navigate(["principal"], extras);
+  }
 
 }
